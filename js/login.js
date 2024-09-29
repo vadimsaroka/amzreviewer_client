67320f3bd07119481faed49ee307fc36286979c2
Vadim Saroka <33795637+vadimsaroka@users.noreply.github.com>
2024-09-28 19:37:35 -0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,31 +1,5 @@
 import axios from "axios";
 
-const loginBtn = document.querySelector("#login");
-
-loginBtn.addEventListener("click", e => {
-    e.preventDefault();
-    console.log(e);
-    // try {
-    //   const res = await axios({
-    //     method: 'POST',
-    //     url: '/api/v1/users/login',
-    //     data: {
-    //       email,
-    //       password
-    //     }
-    //   });
-  
-    //   if (res.data.status === 'success') {
-    //     showAlert('success', 'Logged in successfully!');
-    //     window.setTimeout(() => {
-    //       location.assign('/');
-    //     }, 1500);
-    //   }
-    // } catch (err) {
-    //   showAlert('error', err.response.data.message);
-    // }
-})
-
 export const login = async (email, password) => {
     try {
       const res = await axios({
